27b93e50a54493c611afe32c8021b0db7e3276e2
faketooth <faketooth@faketoothdeMacBook-Pro.local>
2017-12-01 19:57:15 +0800

    keep cache sync with disk

--- save_by_question.py
+++ save_by_question.py
@@ -3,31 +3,47 @@ from zhihu_oauth import ZhihuClient
 import sys
 import time
 import random
+import os
+
+
+def get_cache(dir):
+    cache_file = os.path.join(dir, 'cache')
+    if os.path.exists(cache_file):
+        with open(cache_file, 'r') as f:
+            lines = f.readlines()
+        return set([int(line) for line in lines])
+    return set()
+
 
 def main(question_id):
     question = client.question(int(question_id))
     print("start to save answers of {}".format(question.title.encode('utf-8')))
 
-    answer_cache = set()
+    answer_cache = get_cache(question.title)
+    cache_file = open(os.path.join(question.title, 'cache'), 'a')
     for answer in question.answers:
         if answer.id in answer_cache:
+            print("%s's answer has been saved." % answer.author.name)
+            time.sleep(1)
             continue
-        print(answer.author.name, answer.voteup_count)
+        print(answer.author.name, answer.id, answer.voteup_count)
         filename = "%s_%s" % (answer.author.name, answer.id)
         try:
             answer.save(question.title, filename)
         except GetDataErrorException:
             client.login()
+        cache_file.write("%s\n" % answer.id)
+        cache_file.flush()
         answer_cache.add(answer.id)
         sleep_time = random.randint(1, 3)
         time.sleep(sleep_time)
-
+    cache_file.close()
 
 
 if __name__ == '__main__':
     client = ZhihuClient()
     client.load_token('token.pkl')
-    if len(sys.argv)<2:
+    if len(sys.argv) < 2:
         print("python save_by_question.py question_id")
         sys.exit(-1)
     main(sys.argv[1])

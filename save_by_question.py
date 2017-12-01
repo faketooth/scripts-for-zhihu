99e37986f21203d1f1e9501e0887c0abade9ce06
faketooth <faketooth@faketoothdeMacBook-Pro.local>
2017-12-01 15:52:32 +0800

    save by question id with random sleep seconds in case abnormal network traffic detection

---
+++ save_by_question.py
@@ -0,0 +1,25 @@
+from __future__ import print_function
+from zhihu_oauth import ZhihuClient
+import sys
+import time
+import random
+
+def main(question_id):
+    question = client.question(int(question_id))
+    print("start to save answers of {}".format(question.title.encode('utf-8')))
+
+    for answer in question.answers:
+        print(answer.author.name, answer.voteup_count)
+        answer.save(question.title)
+        sleep_time = random.randint(1, 3)
+        time.sleep(sleep_time)
+
+
+
+if __name__ == '__main__':
+    client = ZhihuClient()
+    client.load_token('token.pkl')
+    if len(sys.argv)<2:
+        print("python save_by_question.py question_id")
+        sys.exit(-1)
+    main(sys.argv[1])

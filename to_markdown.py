0ca434f82a8ac83c7b56df567e20454e2d6d3b13
faketooth <faketooth@faketoothdeMacBook-Pro.local>
2017-12-06 17:45:54 +0800

    convert html to markdown file, and replace picture url with local downloaded file.

--- to_markdown.py
+++ to_markdown.py
@@ -2,6 +2,30 @@ from __future__ import print_function
 import html2text
 import sys
 import os
+import re
+import urllib
+
+
+def replace_with_local_picture(mdfile):
+    pic_prefix = mdfile.split(".")[0]
+    filename = pic_prefix.split("/")[-1]
+    answer_id = mdfile.split("_")[1]
+    pic_prefix = pic_prefix.replace(filename, answer_id)
+    start_tag = re.compile("\!\[\]\(http.*?\)")
+    with open(mdfile) as f:
+        lines = f.readlines()
+    new_lines = []
+    for line in lines:
+        url_pos_pairs = [(m.start(), m.end())
+                         for m in re.finditer(start_tag, line)]
+        for start, end in url_pos_pairs:
+            pic_url = line[start+4:end-1]
+            pic_filename = "%s_%s" % (pic_prefix, pic_url.split("/")[-1])
+            urllib.urlretrieve(pic_url, pic_filename)
+            line = line.replace(pic_url, pic_filename.split("/")[-1])
+        new_lines.append(line.decode('utf-8'))
+    with open(mdfile, 'w') as f:
+        f.write(''.join(new_lines).encode("utf-8"))
 
 
 def convert_file(filename, path=''):
@@ -13,8 +37,12 @@ def convert_file(filename, path=''):
         lines = f.readlines()
     markdown = html2text.html2text(''.join(lines).decode('utf-8'))
 
-    with open("%s.md" % full_path.split(".")[0].decode('utf-8'), 'w') as f:
+    md_filename = "%s.md" % full_path.split(".")[0].decode('utf-8')
+    with open(md_filename, 'w') as f:
         f.write(markdown.encode('utf-8'))
+
+    if download_picture:
+        replace_with_local_picture(md_filename)
 
 
 if __name__ == '__main__':
@@ -22,6 +50,9 @@ if __name__ == '__main__':
         print("python to_markdown.py html_file")
         sys.exit(-1)
     param = sys.argv[1]
+    download_picture = False
+    if len(sys.argv) == 3:
+        download_picture = True
     if os.path.isfile(param):
         convert_file(param)
         sys.exit(0)
